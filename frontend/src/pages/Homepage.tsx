25ec16c65fddc6eefa5271df85dae9a0a11d5705
NickDDDDDD <101779199+NickDDDDDD@users.noreply.github.com>
2025-05-31 18:32:57 +1000

    demo stomp client on rabbit mq

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,7 +1,82 @@
+import { connectStomp, subscribeToTopic, sendMessage } from "../stompClient";
+import React, { useEffect, useState } from "react";
+
 const Homepage = () => {
+  const [topic, setTopic] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    connectStomp();
+  }, []);
+
+  const handleSubscribe = () => {
+    if (topic) {
+      subscribeToTopic(topic, (msg) => {
+        console.log("Received message:", msg.body);
+        setMessages((prevMessages) => [...prevMessages, msg.body]);
+      });
+    } else {
+      console.warn("⚠️ Topic cannot be empty");
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (topic && message) {
+      sendMessage(topic, message);
+      setMessages((prevMessages) => [...prevMessages, message]);
+      console.log("Message sent:", message);
+    } else {
+      console.warn("⚠️ Topic and message cannot be empty");
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "topic") {
+      setTopic(value);
+    } else if (name === "message") {
+      setMessage(value);
+    }
+  };
+
   return (
-    <div className="h-full w-full bg-neutral-900 text-2xl text-neutral-200">
-      Homepage
+    <div className="flex h-full w-full flex-col gap-4 bg-neutral-900 text-2xl text-neutral-100">
+      <div>
+        <h2> current topic</h2>
+        <p>{topic}</p>
+      </div>
+      <div>
+        <label>
+          subscribe to topic:
+          <input
+            type="text"
+            name="topic"
+            className="bg-neutral-100 text-black"
+            onChange={handleInputChange}
+          />
+        </label>
+        <button className="bg" onClick={handleSubscribe}>
+          subscribe
+        </button>
+      </div>
+
+      <div>
+        <label>
+          message:
+          <input
+            type="text"
+            name="message"
+            className="bg-neutral-100 text-black"
+            onChange={handleInputChange}
+          />
+        </label>
+        <button onClick={handleSendMessage}>send</button>
+      </div>
+      <div>
+        <h2>messages</h2>
+        <p>{messages}</p>
+      </div>
     </div>
   );
 };
