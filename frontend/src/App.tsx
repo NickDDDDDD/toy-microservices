055f9bc332209d0687ad06d453f712ba878732b2
NickDDDDDD <101779199+NickDDDDDD@users.noreply.github.com>
2025-05-31 17:25:07 +1000

    init frontend
    - vite react (ts + swc)
    -  tailwind
    -  tailwind merge
    - tailwind prettier

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <div className="w-dvw h-dvh">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div className="h-full w-full bg-neutral-900 text-2xl text-neutral-200">
+      Homepage
+    </div>
+  );
+};
+
+export default Homepage;
